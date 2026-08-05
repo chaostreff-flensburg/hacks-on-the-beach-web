735a563ebfebc78bb03cb76d03f1bb64326c7bca
scammo <sbrinkmann@54gradsoftware.de>
2026-08-05 21:04:22 +0200

    fix year and newsletter

--- content/_components/Newsletter.tsx
+++ content/_components/Newsletter.tsx
@@ -3,7 +3,7 @@ export default () => (
     <h2 id="newsletter-anmelden">Newsletter</h2>
     <form
       method="post"
-      action="https://newsletter.chaostreff-flensburg.de/subscription/form"
+      action="https://newsletter.c3fl.de/subscription/form"
       class=""
     >
       <input
